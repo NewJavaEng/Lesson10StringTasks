ae746ca1752b24179eccce4375a8f94e84819fb0
NewJavaEng <32841342+NewJavaEng@users.noreply.github.com>
2017-11-13 14:20:47 +0300

    Add files via upload

---
+++ taskone/Main.java
@@ -0,0 +1,14 @@
+package by.http.string.taskone;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Task1 myStr = new Task1("Soon is the weekends but we will learn Java");
+		myStr.replStr();
+
+	}
+	
+	
+
+}
---
+++ taskone/Task1.java
@@ -0,0 +1,39 @@
+package by.http.string.taskone;
+
+//В  каждом слове текста 3-ю букву заменить заданным символом 'Z'. Если k больше длины слова, корректировку не выполнять.
+public class Task1 {
+
+	private String str;
+
+	public Task1(String str) {
+		this.str = str;
+	}
+
+	public String getStr() {
+		return str;
+	}
+
+	public void setStr(String str) {
+		this.str = str;
+	}
+
+	public String replStr() {
+		String[] result = str.split(" ");
+		String startVal = "";
+		for (int i = 0; i < result.length; i++) {
+			if (result[i].length() >= 3) {
+				char[] myStrChar = result[i].toCharArray();
+				myStrChar[2] = 'Z';
+				result[i] = new String(myStrChar);
+
+			}
+
+		}
+		for (String eachVal : result) {
+			startVal += eachVal + " ";
+		}
+		System.out.println(startVal);
+		return startVal;
+	}
+
+}
