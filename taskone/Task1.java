9fab9957f2eb1115d634265f56ca4f2dacb355f1
NewJavaEng <32841342+NewJavaEng@users.noreply.github.com>
2017-11-13 18:50:35 +0300

    Add files via upload

--- taskone/Task1.java
+++ taskone/Task1.java
@@ -9,14 +9,6 @@ public class Task1 {
 		this.str = str;
 	}
 
-	public String getStr() {
-		return str;
-	}
-
-	public void setStr(String str) {
-		this.str = str;
-	}
-
 	public String replStr() {
 		String[] result = str.split(" ");
 		String startVal = "";
